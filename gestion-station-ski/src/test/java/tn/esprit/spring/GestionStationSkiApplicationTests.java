ca69a86b0c80039548d8f393376a888d6c01a31d
sarraai <129076485+sarraai@users.noreply.github.com>
2024-11-04 12:34:17 +0100

    Update GestionStationSkiApplicationTests.java

--- gestion-station-ski/src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ gestion-station-ski/src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -1,13 +1,79 @@
-package tn.esprit.spring;
+package tn.esprit.spring.entities;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class GestionStationSkiApplicationTests {
+public class SkierTest {
 
-	@Test
-	void contextLoads() {
-	}
+    private Skier skier;
 
+    @BeforeEach
+    public void setUp() {
+        skier = new Skier();
+    }
+
+    @Test
+    public void testSetAndGetNumSkier() {
+        Long expectedNumSkier = 1L;
+        skier.setNumSkier(expectedNumSkier);
+        assertEquals(expectedNumSkier, skier.getNumSkier());
+    }
+
+    @Test
+    public void testSetAndGetFirstName() {
+        String expectedFirstName = "John";
+        skier.setFirstName(expectedFirstName);
+        assertEquals(expectedFirstName, skier.getFirstName());
+    }
+
+    @Test
+    public void testSetAndGetLastName() {
+        String expectedLastName = "Doe";
+        skier.setLastName(expectedLastName);
+        assertEquals(expectedLastName, skier.getLastName());
+    }
+
+    @Test
+    public void testSetAndGetDateOfBirth() {
+        LocalDate expectedDateOfBirth = LocalDate.of(1990, 1, 1);
+        skier.setDateOfBirth(expectedDateOfBirth);
+        assertEquals(expectedDateOfBirth, skier.getDateOfBirth());
+    }
+
+    @Test
+    public void testSetAndGetCity() {
+        String expectedCity = "Paris";
+        skier.setCity(expectedCity);
+        assertEquals(expectedCity, skier.getCity());
+    }
+
+    @Test
+    public void testSetAndGetSubscription() {
+        Subscription subscription = mock(Subscription.class);
+        skier.setSubscription(subscription);
+        assertEquals(subscription, skier.getSubscription());
+    }
+
+    @Test
+    public void testSetAndGetPistes() {
+        Set<Piste> expectedPistes = new HashSet<>();
+        expectedPistes.add(mock(Piste.class));
+        skier.setPistes(expectedPistes);
+        assertEquals(expectedPistes, skier.getPistes());
+    }
+
+    @Test
+    public void testSetAndGetRegistrations() {
+        Set<Registration> expectedRegistrations = new HashSet<>();
+        expectedRegistrations.add(mock(Registration.class));
+        skier.setRegistrations(expectedRegistrations);
+        assertEquals(expectedRegistrations, skier.getRegistrations());
+    }
 }
